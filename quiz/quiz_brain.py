816e32bb2b5df03b53e1ba504ad814f7463f7297
amityadav341 <rockamit341@gmail.com>
2021-06-26 14:09:57 +0530

    upladed quiz and turtle racing game

---
+++ quiz/quiz_brain.py
@@ -0,0 +1,27 @@
+class QuizBrain:
+    def __init__(self,q_list):
+        self.question_number=0
+        self.question_list=q_list
+        self.score=0
+    def still_has_question(self):
+        if self.question_number==len(self.question_list):
+            return False
+        else:
+           return True
+    def next_question(self):
+        current_question=self.question_list[self.question_number]
+        current_question_ans=self.question_list[self.question_number].answer
+        self.question_number+=1
+        user_ans=input(f"Q{self.question_number}:{current_question.text} (True/False)?: ")
+        self.check_ans(user_ans,current_question_ans)
+    def check_ans(self,user_ans,curr_ans):
+          if user_ans.lower()==curr_ans.lower():
+              self.score+=1
+              print("that's correct answer! ")
+          else:
+              print(f"that's wrong answer! Correct answer is {curr_ans}")
+         
+          print(f"Your current score is {self.score}/{self.question_number}")
+
+         
+        
---
+++ turtle/main.py
@@ -0,0 +1,49 @@
+from turtle import Turtle,Screen, pencolor, title
+import random
+
+screen=Screen()
+
+colors=["violet","blue","green","yellow","orange","red"]
+
+y_pos=[-200,-125,-50,25,100,175]
+
+screen.setup(width=500,height=500)
+
+all_turt=[]
+
+is_on=True
+
+user_bet=screen.textinput(title="Make your bet",prompt="which color turtle will win the race? Enter your choice: \n violet,blue,green,yellow,orange,red")
+if user_bet:
+    for i in range(0,6):
+        tim=Turtle(shape="turtle")
+        tim.color(colors[i])
+        tim.penup()
+        tim.goto(x=-200,y=y_pos[i])
+        all_turt.append(tim)
+    while is_on:
+    
+        for tim in all_turt:
+            if tim.xcor()>220:
+
+                is_on=False
+
+                win_turt=tim.pencolor()
+
+                if user_bet==win_turt:
+
+                    print(f"Congratulation! you win")
+                    break
+                    
+                else:
+
+                    print(f"You loose! Winning Turtle is {win_turt} ")
+                    break
+
+            dis=random.randint(1,10)
+
+            tim.forward(dis)
+else:
+    print("Enter your bet")      
+
+screen.exitonclick()
